27a1dc2278ac32a12d8b2afb51729441eb51a4af
cohlerCoded <jeffcohler@cohlercoded.com>
2021-08-10 10:50:07 -0700

    rendering list of posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const BlogContext = React.createContext()
 
+const blogPosts = [
+  { title: 'meow', user: 'onlysaysmeow', body: 'meow' },
+  { title: 'Boo Everyone!!!', user: 'hater', body: "I don't like people!" },
+  { title: 'Woof!', user: 'imadog', body: 'Woof woof!' },
+]
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider value={5}>{children}</BlogContext.Provider>
+  return (
+    <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
+  )
 }
 
 export default BlogContext
--- src/screens/IndexScreen.jsx
+++ src/screens/IndexScreen.jsx
@@ -1,12 +1,18 @@
 import React, { useContext } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 import BlogContext from '../context/BlogContext'
 
 export default function IndexScreen() {
-  const value = useContext(BlogContext)
+  const blogPosts = useContext(BlogContext)
   return (
     <View>
-      <Text>Index Screen {value}</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={(blogPost, i) => `${i}`}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>
+        }}
+      />
     </View>
   )
 }
