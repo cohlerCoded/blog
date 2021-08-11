8a85eae6f3a514f740c3eb0e8f5312f4a2203ea2
cohlerCoded <jeffcohler@cohlercoded.com>
2021-08-11 09:19:01 -0700

    switch to useReducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 
 const BlogContext = React.createContext()
-
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_BLOGPOST':
+      return [...state, action.payload]
+    default:
+      return state
+  }
+}
 const blogs = [
   { title: 'meow', user: 'onlysaysmeow', body: 'meow' },
   { title: 'Boo Everyone!!!', user: 'hater', body: "I don't like people!" },
@@ -9,9 +16,11 @@ const blogs = [
 ]
 
 export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState(blogs)
+  const [blogPosts, dispatch] = useReducer(blogReducer, blogs)
 
-  const addBlogPost = (blog) => setBlogPosts([...blogPosts, blog])
+  const addBlogPost = (blog) => {
+    dispatch({ type: 'ADD_BLOGPOST', payload: blog })
+  }
 
   return (
     <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
