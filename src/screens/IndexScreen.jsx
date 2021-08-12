ceaccb9e5dc5eaffd635891d07312f19035c1091
cohlerCoded <jeffcohler@cohlercoded.com>
2021-08-11 18:38:37 -0700

    some styling

--- src/screens/IndexScreen.jsx
+++ src/screens/IndexScreen.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native'
 import { Context } from '../context/BlogContext'
+import { Feather } from '@expo/vector-icons'
 
 export default function IndexScreen() {
   const { state, addBlogPost } = useContext(Context)
@@ -16,11 +17,31 @@ export default function IndexScreen() {
         data={state}
         keyExtractor={(blogPost, i) => `${i}`}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Feather style={styles.trashIcon} name='trash-2' />
+            </View>
+          )
         }}
       />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderBottomWidth: 1,
+    borderColor: 'grey',
+  },
+  title: {
+    fontSize: 18,
+  },
+  trashIcon: {
+    fontSize: 24,
+  },
+})
